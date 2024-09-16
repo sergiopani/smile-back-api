2daaaa8886ba96519b6fcc85c577c90c84e54398
Sergio Paniagua <xpanisergio@gmail.com>
2024-09-16 19:16:52 +0200

    users from web

--- main.py
+++ main.py
@@ -5,8 +5,39 @@ users = [
         "id": 1,
         "name": "smile",
         "pass": "1234"
+        "email": "xpanisergio@gmail.com"
     }
 ]
+
+
+users_from_web = [
+    {
+        "id": 1,
+        "name": "Sergio",
+        "pass": "1234"
+    },
+    {
+        "id": 1,
+        "name": "Jordi",
+        "pass": "1234"
+    },
+    {
+        "id": 1,
+        "name": "Anonimo",
+        "pass": "1234"
+    },
+    {
+        "id": 1,
+        "name": "Anonimo",
+        "pass": "1234"
+    }
+]
+
+
+class LoginRequest(BaseModel):
+    email: str
+    password: str
+
 
 app = FastAPI()
 
@@ -15,8 +46,13 @@ async def root():
     return {"message": "Hello World"}
 
 
-@app.get("/users")
-async def get_users():
-    return JSONResponse(content=users)
+@app.post("/login")
+async def login(login_request: LoginRequest):
+    for user in users:
+        if user["email"] == login_request.email and user["pass"] == login_request.password:
+            return JSONResponse(content={"message": "Login successful", "user_id": user["id"]})
+    raise HTTPException(status_code=401, detail="Invalid credentials")
 
-
+@app.post("/get_users_from_web")
+async def get_users_from_web():
+    return JSONResponse(content={"users": users_from_web})
