32e8b2424fd289bf2f07da7593982e4ccfa02c50
Sergio Paniagua <xpanisergio@gmail.com>
2024-09-16 19:24:20 +0200

    fixing

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from fastapi.responses import JSONResponse
+from pydantic import BaseModel
+
 users = [
     {
         "id": 1,
